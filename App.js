c1c0553bdf2d076b88d8e1e1a6a0fee8908903b8
sangkwun <wer2774@gmail.com>
2021-05-22 14:59:12 +0900

    add amplify config

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   return (
