a6aac5691dbef538f939232ef51e1044551f68b0
Sangkwun <wer2774@gmail.com>
2021-05-22 15:52:57 +0900

    use AsyncStorage to store nickname

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-import React from 'react';
-import { Text, View } from 'react-native';
-import { API, graphqlOperation} from 'aws-amplify'
-import { createTodo, updateTodo, deleteTodo } from '../../graphql/mutations';
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+});
 
 
 export default function Main() {
+
+  const [ userName, setUserName ] = useState("");
+
+  async function loadUserName(){
+    const res = await AsyncStorage.getItem('@userName')
+    if(res!==null){
+      setUserName(res)
+    }
+  }
+
+  function configureUserName(){
+    AsyncStorage.setItem('@userName', userName)
+  }
+  useEffect(( )=>{
+    loadUserName();
+  }, [])
+
+  useEffect(( )=>{
+    configureUserName();
+  }, [userName])
+
   return (
     <View>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!</Text>
+      <TextInput placeholder="Nickname" style={styles.input} value={userName} onChangeText={setUserName} />
+      {userName.length>0?<Button title="Start game" onPress={configureUserName}/>:null}
     </View>
   );
 }
+
+
